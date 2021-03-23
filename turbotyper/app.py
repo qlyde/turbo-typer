277a5c3a8b4559ac42a72da3c6c2cd5312495e88
James Kroeger <jameskroeger123@gmail.com>
2021-03-24 09:08:55 +1100

    refactor

--- turbotyper/app.py
+++ turbotyper/app.py
@@ -8,7 +8,7 @@ from turbotyper.ui.MainWindow import MainWindow
 
 def main():
     app = QApplication(sys.argv)
-    app.setStyleSheet(open(f"{Const.STYLES}").read())
+    app.setStyleSheet(open(Const.STYLES).read())
     mw = MainWindow()
     mw.show()
     sys.exit(app.exec_())
